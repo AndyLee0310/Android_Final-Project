954b3eea7d692e11dd364203c1e49d2ea9817102
AndyLee <t108590001@ntut.edu.tw>
2021-06-14 23:30:31 +0800

    add Welcome activity

---
+++ app/src/main/java/me/mahakagg/notesroom/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package me.mahakagg.notesroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        mHandler.sendEmptyMessageDelayed(GOTO_MAIN_ACTIVITY, 2000); //2秒跳轉
+    }
+    private static final int GOTO_MAIN_ACTIVITY = 0;
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            switch(msg.what) {
+                case GOTO_MAIN_ACTIVITY:
+                    Intent intent = new Intent();
+                    intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+}
